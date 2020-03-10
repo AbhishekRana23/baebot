c1a1653ccc080c73b27420d04650fc312505df45
Abhishek Rana <40822125+AbhishekRana23@users.noreply.github.com>
2020-03-10 12:01:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@
 // running to a Command instance. When started, an owner
 // ID should be given.
 
+
+
+
 var path = require("path");
 var fs = require("fs");
 var botgram = require("botgram");
@@ -510,3 +513,16 @@ bot.command("help", function (msg, reply, next) {
 bot.command(function (msg, reply, next) {
   reply.reply(msg).text("Invalid command.");
 });
+
+// Load the http module to create an http server.
+const http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Last, but not least, listen on port 8080
+// The environment variable PORT is automatically defined and equals to 8080
+server.listen(process.env.PORT, '0.0.0.0');
